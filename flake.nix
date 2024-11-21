4f864c3920f0b84d9c2d74ccf431c48fbaa345b8
Sergey Kazenyuk <kazenyuk@pm.me>
2024-11-21 04:23:06 +0300

    flake: init nixosConfiguration

--- flake.nix
+++ flake.nix
@@ -41,6 +41,68 @@
       default = import ./modules/default.nix;
     };
 
+    nixosConfigurations = let
+      system = "aarch64-linux";
+    in {
+      pwnagotchi = nixos-raspberrypi.inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = inputs // { inherit system; };
+        modules = [
+          ({ config, pkgs, ... }: {
+            hardware.raspberry-pi.config.pi02 = {
+              # https://github.com/NixOS/nixpkgs/issues/216886#issuecomment-1435758905
+              # Give up VRAM for more Free System Memory
+              options = {
+                # - Disable camera which automatically reserves 128MB VRAM
+                start_x = {
+                  enable = true;
+                  value = 0;
+                };
+                # - Reduce allocation of VRAM to 16MB minimum for non-rotated (32MB for rotated)
+                gpu_mem = {
+                  enable = true;
+                  value = 0;
+                };
+              };
+            };
+          })
+          ({ config, pkgs, ... }: { # ethernet "gadget" mode
+            boot.kernel.sysctl = {
+              # In case networkd/... won't remove the gadget route when it isn't connected
+              # https://github.com/charkster/rpi_gadget_mode
+              # https://www.marcusfolkesson.se/til/ignore_routes_with_linkdown/
+              "net.ipv4.conf.all.ignore_routes_with_linkdown" = 1;
+            };
+            networking.interfaces.usb0.ipv4.addresses = [ {
+              address = "10.0.0.2";
+              prefixLength = 24;
+            } ];
+            boot.kernelModules = [ "dwc2" "g_ether" ];  # modules-load=dwc2,g_ether
+            hardware.raspberry-pi.config.all.dt-overlays = {
+              dwc2 = {
+                enable = true;
+                params = {};
+              };
+            };
+          })
+
+          ({ config, pkgs, lib, ... }: {
+            nixpkgs.overlays = [ self.overlays.default ];
+
+            networking.hostName = "pwnagotchi";
+            boot.loader.grub.enable = false;
+          })
+          self.nixosModules.default
+
+          nixos-raspberrypi.lib.inject-overlays-global
+          nixos-raspberrypi.lib.inject-overlays
+        ] ++ (with nixos-raspberrypi.nixosModules; [
+          raspberry-pi-4.base
+          sd-image
+        ]);
+      };
+    };
+
     overlays = {
       default = import ./overlays/default.nix;
     };
